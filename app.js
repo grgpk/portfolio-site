da2f55425d708ed77599de656cef392da0e15fcb
Giorgi <grgpaikidze@gmail.com>
2025-10-24 21:27:37 +0400

    feat: add responsive navigation

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburgerContainer = document.querySelector("#header__main-nav");
+const hamburger = document.querySelector(".header__main-nav--hamburger");
+const links = document.querySelectorAll(".header__main-nav--links li");
+
+hamburger.addEventListener("click", (e) => {
+  hamburgerContainer.classList.toggle("clicked");
+  links.forEach((li) => li.classList.toggle("fade"));
+});
